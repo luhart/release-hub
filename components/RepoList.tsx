a61dcdd253d05fe34712a4bf70dd2a00eed8f3c2
Luke Hartman <ljhartman10@gmail.com>
2021-05-12 14:21:58 -0700

    Consolidate RepoListItem.tsx -> RepoList.tsx

--- components/RepoList.tsx
+++ components/RepoList.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import RepoItem from './RepoListItem'
 import { Repo } from '../interfaces'
 
 type Props = {
@@ -9,8 +8,9 @@ type Props = {
 const RepoList = ({ repos }: Props) => (
   <ul className="mt-8 space-y-3">
     {repos.map((repo) => (
-      <li key={repo.url}>
-        <RepoItem repo={repo} />
+      <li key={repo.tagName} className="bg-white shadow overflow-hidden rounded-md px-6 py-4">
+        {repo.repoName} - {repo.tagName}
+        ... other details available onClick 
       </li>
     ))}
   </ul>
